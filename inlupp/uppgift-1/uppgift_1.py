29665bc890e36187870e30d129bf7e3cc81faf88
nadiamessary <nadia.messary.data24sto@edu.tucsweden.se>
2025-01-01 19:49:15 +0100

    Add files via upload

---
+++ inlupp/uppgift-1/uppgift_1.py
@@ -0,0 +1,11 @@
+# Uppgift 1
+# Skapa en funktion is_odd(x) som returnerar True om x är udda och False om x är jämnt.
+# def funktions_namn(variabel_namn: datatyp) -> returtyp:
+# Exempel: def is_odd(x: int) -> bool:
+# Förklaring: Funktionens namn är is_odd och tar en parameter x av datatypen int. Funktionen returnerar en bool.
+
+def funktions_namn(variabel_namn: datatyp) -> returtyp:
+    """
+    Skriv beskrivning här.
+    """
+    pass # Ta bort denna rad och skriv din kod här
